c70d59a058816641bb9390bd0edbe069163e132f
Dakic, Aleksandar <aleksandar.dakic@zuehlke.com>
2024-01-09 10:25:22 +0100

    Fix test resurces

--- custom_filter_test/server.js
+++ custom_filter_test/server.js
@@ -14,22 +14,22 @@ async function run() {
   app.post("/endpoints.Greeter/SayHello", (req, res) => {
     var message = "Hello";
     console.log('hello');
-    // if (
-    //   req.body !== undefined &&
-    //   req.body instanceof Buffer &&
-    //   req.body.length != 0
-    // ) {
-    //   const HelloRequest = root.lookupType("endpoints.HelloRequest");
-    //   const helloReq = HelloRequest.decode(req.body);
-    //   message += " " + helloReq.name;
-    // }
+    if (
+      req.body !== undefined &&
+      req.body instanceof Buffer &&
+      req.body.length != 0
+    ) {
+      const HelloRequest = root.lookupType("endpoints.HelloRequest");
+      const helloReq = HelloRequest.decode(req.body);
+      message += " " + helloReq.name;
+    }
 
-    // res.header("Content-Type", "application/grpc");
+    res.header("Content-Type", "application/grpc");
 
-    // const HelloReply = root.lookupType("endpoints.HelloReply");
-    // res.send(HelloReply.encode({ message: message }).finish());
-    // res.end();
-    // console.log('end');
+    const HelloReply = root.lookupType("endpoints.HelloReply");
+    res.send(HelloReply.encode({ message: message }).finish());
+    res.end();
+    console.log('end');
   });
 
   app.listen(8080);
